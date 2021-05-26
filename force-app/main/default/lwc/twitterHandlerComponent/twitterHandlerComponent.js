f11d7508ab6f2d2471c02408e0f32d8e0738311e
Prakash Reddyvari <prakashreddy653@gmail.com>
2021-05-26 11:43:20 -0400

    Add Toast Message

--- force-app/main/default/lwc/twitterHandlerComponent/twitterHandlerComponent.js
+++ force-app/main/default/lwc/twitterHandlerComponent/twitterHandlerComponent.js
@@ -1,20 +1,52 @@
 import { LightningElement,api,wire } from 'lwc';
 import { getRecord } from 'lightning/uiRecordApi';
-const Fields=['Account.TwitterID__c'] // Stores Twitter TID
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+const FIELDS = ['Account.TwitterID__c'];
 export default class TwitterHandlerComponent extends LightningElement {
-    @api recordId
-    twitterHandlerId ="SalesforceDevs"
+    @api recordId;
+    twitterHandlerId ="SalesforceDevs";
+    loading = false;
+    sfdcBaseURL;
     get fullTwitterUrl(){
-        return 'https://SFOrgVFDomain/apex/TwitterFeedPage?twitterHandle=${this.twitterHandlerId}'   // Company ORG VF Domain
+        return this.sfdcBaseURL+`/apex/TwitterFeedPage?twitterHandle=${this.twitterHandlerId}`
+    }
+    connectedCallback() {
+        this.loading = true;
+        this.sfdcBaseURL = window.location.origin;
+    }
+    renderedCallback() {
+        this.loading = false;
     }
 
-    @wire(getRecord, {recordId:'recordId', fields:Fields})
-    wiredRecord({error,data}){
-        if(data){
-            this.twitterHandlerId = data.fields.TwitterID__c.value
+    @wire(getRecord, { recordId: '$recordId', fields: FIELDS })
+    wiredRecord({ error, data }) {
+        if (error) {
+            let message = 'Unknown error';
+            if (Array.isArray(error.body)) {
+                message = error.body.map(e => e.message).join(', ');
+            } else if (typeof error.body.message === 'string') {
+                message = error.body.message;
+            }
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Error loading Twitter Account Page',
+                    message,
+                    variant: 'error',
+                }),
+            );
+        } else if (data) {
+            let message = 'Success!';
+            if(data.fields.TwitterID__c.value){
+                this.twitterHandlerId = data.fields.TwitterID__c.value;
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Twitter Account ID updated Successfully',
+                        message,
+                        variant: 'success',
+                    }),
+                );
+            }           
         }
-        if(error){
-            console.error(error)
-        }
-    }
+    }    
 }
